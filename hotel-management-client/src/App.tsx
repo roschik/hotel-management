7e5be7e81213c02c1fb3248a6bd9a9ef2961356d
marina <mirko_marina@mail.ru>
2025-08-30 23:44:59 +0700

    Исправления для деплоя

--- hotel-management-client/src/App.tsx
+++ hotel-management-client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline, Box } from '@mui/material';
 import { Provider } from 'react-redux';
